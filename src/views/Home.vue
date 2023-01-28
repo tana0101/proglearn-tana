b3842568457d32a81d18e391b182d8cc0b04df62
tana0101 <xenone0101@gmail.com>
2023-01-29 03:46:13 +0800

    Revert to the original version, but cannot build normally

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,23 +4,23 @@ import headerVue from '@/components/header.vue';
 import { onMounted, ref } from 'vue'
 import { ElScrollbar } from 'element-plus'
 
-// const max = ref(0)
-// const value = ref(0)
-// const innerRef = ref<HTMLDivElement>()
-// const scrollbarRef = ref<InstanceType<typeof ElScrollbar>>()
-// /*scroll*/
-// onMounted(() => {
-//   max.value = innerRef.value!.clientHeight - scrollbarRef.value!.$el.clientHeight
-// })
-// const inputSlider = (value: number) => {
-//   scrollbarRef.value!.setScrollTop(value)
-// }
-// const scroll = ({ scrollTop }) => {
-//   value.value = scrollTop
-// }
-// const formatTooltip = (value: number) => {
-//   return `${value} px`
-// }
+const max = ref(0)
+const value = ref(0)
+const innerRef = ref<HTMLDivElement>()
+const scrollbarRef = ref<InstanceType<typeof ElScrollbar>>()
+/*scroll*/
+onMounted(() => {
+  max.value = innerRef.value!.clientHeight - scrollbarRef.value!.$el.clientHeight
+})
+const inputSlider = (value: number) => {
+  scrollbarRef.value!.setScrollTop(value)
+}
+const scroll = ({ scrollTop }) => {
+  value.value = scrollTop
+}
+const formatTooltip = (value: number) => {
+  return `${value} px`
+}
 
 </script>
 
@@ -32,16 +32,16 @@ import { ElScrollbar } from 'element-plus'
       </el-header>
       <el-main>
 
-        <!-- <el-scrollbar ref="scrollbarRef" always @scroll="scroll"> -->
+        <el-scrollbar ref="scrollbarRef" always @scroll="scroll">
           <div ref="innerRef">
             <router-view></router-view>
           </div>
-        <!-- </el-scrollbar> -->
+        </el-scrollbar>
 
         <!--<router-view></router-view>-->
       </el-main>
       <el-footer>
-        <!-- <el-slider v-model="value" :max="max" :format-tooltip="formatTooltip" @input="inputSlider" /> -->
+        <el-slider v-model="value" :max="max" :format-tooltip="formatTooltip" @input="inputSlider" />
       </el-footer>
     </el-container>
   </div>

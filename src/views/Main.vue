216930469da6d4e0f84d72b6636f2221ae299326
tana0101 <xenone0101@gmail.com>
2023-01-29 02:48:13 +0800

    to fix getImage function

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,7 +6,7 @@ import studentVue from '@/components/student.vue';
 const props = defineProps(['data'])
 const data = props.data
 const getImage = (value: number) => {
-  return "src/assets/img/board_" + value + ".jpg"
+  return require('@/assets/img/board_' + value + '.jpg')
 }
 
 const show = ref(false)
